427d9db08d7163a3d46b79d56bd813a707511f7b
Adam Neale <adam.neale@worldfirst.com>
2017-07-12 19:48:07 +0100

    Add logging via monolog

--- src/App/DnsUpdateCommand.php
+++ src/App/DnsUpdateCommand.php
@@ -7,10 +7,10 @@ use DigitalOceanV2\DigitalOceanV2;
 use DigitalOceanV2\Entity\DomainRecord;
 use IpResolution\Ip;
 use IpResolution\Resolver\Resolver;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
 
 class DnsUpdateCommand extends Command
 {
@@ -27,101 +27,104 @@ class DnsUpdateCommand extends Command
     private $domainRecordApi;
 
     /**
-     * @var array
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var string[]
      */
     private $domains;
 
     /**
-     * @var SymfonyStyle
+     * @var DomainRecord[]
      */
-    private $io;
+    private $domainRecords;
 
     /**
      * @param Resolver $ipResolver
      * @param DigitalOceanV2 $digitalOceanApi
-     * @param array $domains
+     * @param LoggerInterface $logger
+     * @param string[] $domains
      */
-    public function __construct(Resolver $ipResolver, DigitalOceanV2 $digitalOceanApi, array $domains)
-    {
+    public function __construct(
+        Resolver $ipResolver,
+        DigitalOceanV2 $digitalOceanApi,
+        LoggerInterface $logger,
+        array $domains
+    ) {
         $this->ipResolver = $ipResolver;
         $this->domainRecordApi = $digitalOceanApi->domainRecord();
+        $this->logger = $logger;
         $this->domains = $domains;
 
         parent::__construct();
     }
 
+    /**
+     * @inheritdoc
+     */
     public function configure()
     {
-        $this->setName('dns:update')
-            ->setDescription('Updates DNS records');
+        $this->setName('dns:update')->setDescription('Updates DNS records');
     }
 
+    /**
+     * @inheritdoc
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->io = new SymfonyStyle($input, $output);
-
         try {
             // TODO check if IP already in cache, if so don't update
             $ip = $this->ipResolver->getIp();
 
+            $this->logger->info('Detected a new IP', ['IP' => (string) $ip]);
             foreach ($this->domains as $domainName => $recordNames) {
-                $this->upsertDomainRecords($domainName, $recordNames, $ip);
+                foreach ($recordNames as $recordName) {
+                    $this->upsertDomainRecord($domainName, $recordName, $ip);
+                }
             }
-
         } catch (\Exception $exception) {
-            $this->io->error($exception->getMessage());
+            $this->logger->error($exception->getMessage());
         }
-    }
-
-    /**
-     * @param string $domainName
-     * @param string[] $recordNames
-     * @param Ip $ip
-     */
-    private function upsertDomainRecords(string $domainName, array $recordNames, Ip $ip)
-    {
-        foreach ($this->domainRecordApi->getAll($domainName) as $domainRecord) {
-            if (empty($recordNames)) {
-                break;
-            }
-
-            if (
-                self::ADDRESS_RECORD === $domainRecord->type
-                && in_array($domainRecord->name, $recordNames)
-            ) {
-                $updatedRecord = $this->updateDomainRecord($domainName, $domainRecord, $ip);
-                $this->io->success("Updated the IP for {$updatedRecord->name}.$domainName to $updatedRecord->data");
-                $recordNames = array_diff($recordNames, [$updatedRecord->name]);
-            }
-        }
-
-        foreach ($recordNames as $recordName) {
-            $createdRecord = $this->createDomainRecord($domainName, $recordName, $ip);
-            $this->io->success("Added the IP for {$createdRecord->name}.$domainName to $createdRecord->data");
-        }
-    }
-
-    /**
-     * @param string $domainName
-     * @param DomainRecord $domainRecord
-     * @param Ip $ip
-     *
-     * @return DomainRecord
-     */
-    private function updateDomainRecord(string $domainName, DomainRecord $domainRecord, Ip $ip)
-    {
-        return $this->domainRecordApi->updateData($domainName, $domainRecord->id, (string) $ip);
     }
 
     /**
      * @param string $domainName
      * @param string $recordName
      * @param Ip $ip
-     *
-     * @return DomainRecord
      */
-    private function createDomainRecord(string $domainName, string $recordName, Ip $ip)
+    private function upsertDomainRecord(string $domainName, string $recordName, Ip $ip)
     {
-        return $this->domainRecordApi->create($domainName, self::ADDRESS_RECORD, $recordName, (string) $ip);
+        $domainRecord = $this->fetchDomainRecord($domainName, $recordName);
+        $upsertedRecord = $domainRecord
+            ? $this->domainRecordApi->updateData($domainName, $domainRecord->id, (string) $ip)
+            : $this->domainRecordApi->create($domainName, self::ADDRESS_RECORD, $recordName, (string) $ip);
+
+        $this->logger->info(
+            ($domainRecord ? 'Updated' : 'Inserted') . ' record',
+            ['record' => $upsertedRecord]
+        );
+    }
+
+    /**
+     * @param string $domainName
+     * @param string $recordName
+     *
+     * @return DomainRecord|null
+     */
+    private function fetchDomainRecord(string $domainName, string $recordName)
+    {
+        if (!isset($this->domainRecords[$domainName])) {
+            $this->domainRecords[$domainName] = $this->domainRecordApi->getAll($domainName);
+        }
+
+        foreach ($this->domainRecords[$domainName] as $domainRecord) {
+            if ($recordName === $domainRecord->name && self::ADDRESS_RECORD === $domainRecord->type) {
+                return $domainRecord;
+            }
+        }
+
+        return null;
     }
 }
